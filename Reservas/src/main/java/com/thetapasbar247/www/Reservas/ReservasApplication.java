94e893b4bf546ac3452fb41532c6346d105a4f06
Duo Xu <duoxu1987@gmail.com>
2021-09-20 23:36:30 +0200

    Second Commit

---
+++ Reservas/src/main/java/com/thetapasbar247/www/Reservas/ReservasApplication.java
@@ -0,0 +1,15 @@
+package com.thetapasbar247.www.Reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * Negociador - SpringApplication
+ */
+@SpringBootApplication
+public class ReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasApplication.class, args);
+	}
+
+}
